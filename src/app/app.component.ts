aad9301c4cb6fac518d29fbfb11205d1589bdd82
beardbytes <adi.maheshwari53@gmail.com>
2020-08-24 22:10:08 +0530

    Minor changes to UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,41 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  url = 'http://localhost:8080/EncryptDecrypt/webapi/JsonTest';
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(response => {
+      console.log(response);
+    });
+  }
+
+  // export class AppComponent {
   title = 'EncryptTest';
   encryptMode: boolean;
   textToConvert: string;
   password: string;
   conversionOutput: string;
- 
-  form = new FormGroup({
-    website: new FormControl('', Validators.required)
-  });
-  
-  get f(){
-    return this.form.controls;
-  }
-  
-  submit(){
-    console.log(this.form.value);
-  }
-  constructor() {
-    this.encryptMode = true;
-  }
+
+  // form = new FormGroup({
+  //   website: new FormControl('', Validators.required)
+  // });
+
+  // get f() {
+  //   return this.form.controls;
+  // }
+
+  // submit() {
+  //   console.log(this.form.value);
+  // }
+  // constructor() {
+  //   this.encryptMode = true;
+  // }
 
   changeMode() {
     this.encryptMode = this.encryptMode ? false : true;
@@ -40,17 +49,17 @@ export class AppComponent {
     this.encryptMode = this.encryptMode;
     this.textToConvert = "";
   }
-
-  convertText() {
-    if (this.textToConvert.trim() === "" || this.password.trim() === "") {
-      this.conversionOutput = "Please fill the textboxes."
-      return;
-    }
-    else {
-      if (this.encryptMode) {
-      }
-      else {
-      }
-    }
-  }
 }
+//   convertText() {
+//     if (this.textToConvert.trim() === "" || this.password.trim() === "") {
+//       this.conversionOutput = "Please fill the textboxes."
+//       return;
+//     }
+//     else {
+//       if (this.encryptMode) {
+//       }
+//       else {
+//       }
+//     }
+//   }
+// }
